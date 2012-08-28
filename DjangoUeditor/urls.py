f555bcc50eba1f81e057a3eb7213d3df4d15ba65
zhangfisher <zhwx@hotmail.com>
2012-08-28 12:11:45 +0800

    support django 1.3

--- DjangoUeditor/urls.py
+++ DjangoUeditor/urls.py
@@ -1,5 +1,10 @@
 #coding:utf-8
-from django.conf.urls import patterns, url
+from django import VERSION
+if VERSION[0:2]>(1,3):
+    from django.conf.urls import patterns, url
+else: 
+    from django.conf.urls.defaults import patterns, url
+
 from DjangoUeditor.views import UploadFile,ImageManager,RemoteCatchImage,SearchMovie
 
 urlpatterns = patterns('',
