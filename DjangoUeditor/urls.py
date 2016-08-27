a1805ae1b035a2ec4082d275c0939f2bb1bf1f33
yumendy <yumendy@163.com>
2016-08-27 13:08:14 +0800

    修改了urls.py使其支持django 1.10以上版本 修改了views.py 使其文件存储改为调用storage后台

--- DjangoUeditor/urls.py
+++ DjangoUeditor/urls.py
@@ -1,12 +1,17 @@
-#coding:utf-8
+# coding:utf-8
 from django import VERSION
-if VERSION[0:2]>(1,3):
+from views import get_ueditor_controller
+
+if VERSION[0:2] > (1, 3):
     from django.conf.urls import patterns, url
 else:
     from django.conf.urls.defaults import patterns, url
 
-from views import get_ueditor_controller
-
-urlpatterns = patterns('',
-    url(r'^controller/$',get_ueditor_controller)
-)
+if VERSION[0:2] > (1, 9):
+    urlpatterns = [
+        url(r'^controller/$', get_ueditor_controller)
+    ]
+else:
+    urlpatterns = patterns('',
+                           url(r'^controller/$', get_ueditor_controller)
+                           )
